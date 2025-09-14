b886c55bea628b1f98aefa8d44ecdbb2276c4d99
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-09-15 02:15:28 +0300

    fix logic for increase

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
     setCount(prev => {
       const next = prev + 1;
 
-      return next % 5 === 0 ? next + 100 : next;
+      return prev % 5 === 0 ? next + 100 : next;
     });
   };
 
